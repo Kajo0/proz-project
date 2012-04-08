08605c719c7d302540dfe3cc899b4423e1e3c2d4
kajo100@gmail.com <kajo100@gmail.com@23d1e3b5-b172-319e-1904-7776c5071fde>
2012-04-08 13:04:34 +0000

    fewer text

--- trunk/src/pl/edu/pw/elka/mmarkiew/controller/QueueController.java
+++ trunk/src/pl/edu/pw/elka/mmarkiew/controller/QueueController.java
@@ -35,60 +35,42 @@ public class QueueController implements Runnable {
 		// TODO zmienic na gameaction
 		try {
 			if (event instanceof ViewKeyPress) {
-				if (((ViewKeyPress) event).getKeyCode() == KeyEvent.VK_UP) {
-					if (((ViewKeyPress) event).isPress())
-						player.setYVelocity(-player.getMaxVelocity());
-						else player.setYVelocity(0);
-				}
-				if (((ViewKeyPress) event).getKeyCode() == KeyEvent.VK_DOWN) {
-					if (((ViewKeyPress) event).isPress())
-						player.setYVelocity(player.getMaxVelocity());
-					else player.setYVelocity(0);
-				}
-				if (((ViewKeyPress) event).getKeyCode() == KeyEvent.VK_LEFT) {
-					if (((ViewKeyPress) event).isPress())
-						player.setXVelocity(-player.getMaxVelocity());
-					else player.setXVelocity(0);
-				}
-				if (((ViewKeyPress) event).getKeyCode() == KeyEvent.VK_RIGHT) {
-					if (((ViewKeyPress) event).isPress())
-						player.setXVelocity(player.getMaxVelocity());
-					else player.setXVelocity(0);
-				}
-				if (((ViewKeyPress) event).getKeyCode() == KeyEvent.VK_SPACE) {
-					if (((ViewKeyPress) event).isPress()) {
-						model.plantBomb();
-					}
-				}
-				if (((ViewKeyPress) event).getKeyCode() == KeyEvent.VK_1) {
-					if (((ViewKeyPress) event).isPress()) {
-						model.getPlayer().setBombTimer(1000);
-					}
-				}
-				if (((ViewKeyPress) event).getKeyCode() == KeyEvent.VK_2) {
-					if (((ViewKeyPress) event).isPress()) {
-						model.getPlayer().setBombTimer(2000);
-					}
-				}
-				if (((ViewKeyPress) event).getKeyCode() == KeyEvent.VK_3) {
-					if (((ViewKeyPress) event).isPress()) {
-						model.getPlayer().setBombTimer(3000);
-					}
+				int code = ((ViewKeyPress) event).getKeyCode();
+				boolean pressed = ((ViewKeyPress) event).isPress();
+				switch (code) {
+					case KeyEvent.VK_UP: if (pressed)
+											player.setYVelocity(-player.getMaxVelocity());
+										else player.setYVelocity(0);
+										break;
+					case KeyEvent.VK_DOWN: if (pressed)
+												player.setYVelocity(player.getMaxVelocity());
+											else player.setYVelocity(0);
+											break;
+					case KeyEvent.VK_LEFT: if (pressed)
+												player.setXVelocity(-player.getMaxVelocity());
+											else player.setXVelocity(0);
+											break;
+					case KeyEvent.VK_RIGHT: if (pressed)
+												player.setXVelocity(player.getMaxVelocity());
+											else player.setXVelocity(0);
+											break;
+					case KeyEvent.VK_SPACE: if (pressed)
+												model.plantBomb();
+											break;
+					case KeyEvent.VK_1:
+					case KeyEvent.VK_2:
+					case KeyEvent.VK_3:	if (pressed)
+											player.setBombTimer((code - 48) * 1000);
+										break;
+					case KeyEvent.VK_P: if (pressed)
+											model.switchPause();
+										break;
+					case KeyEvent.VK_ESCAPE: System.exit(0);//TODO Wywalic escape sysexit
 				}
 			}
 		} catch (NullPointerException e) {
 			//TODO wywalic sysouta
 			System.out.println("nie bylo playera przy key evencie");
-		}
-			
-		if (((ViewKeyPress) event).getKeyCode() == KeyEvent.VK_P) {
-			if (((ViewKeyPress) event).isPress())
-				model.switchPause();
-		}
-		
-		//TODO Wywalic escape sysexit
-		if (((ViewKeyPress) event).getKeyCode() == KeyEvent.VK_ESCAPE) {
-			System.exit(0);
 		}
 	}
 
